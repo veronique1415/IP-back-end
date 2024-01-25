26c860a324321eeb9dde966fa8b8fb8c5ed277e2
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-25 17:13:24 -0500

    add base middleware

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const port = process.env.PORT || 8080;
 
 
-
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
